0fbec5f5c69ef4aa1211518fbccfb386e67b8775
Ankush <ankushgazta@gmail.com>
2025-06-25 10:14:31 +0530

    change: Initial Setup

--- database/migrations/2025_06_25_025741_create_orders_table.php
+++ database/migrations/2025_06_25_025741_create_orders_table.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('status', ['pending', 'completed', 'cancelled']);
+            $table->decimal('total_amount', 10, 2);
             $table->timestamps();
         });
     }
@@ -24,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 };
